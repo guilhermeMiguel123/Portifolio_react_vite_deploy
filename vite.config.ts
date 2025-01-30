dfb024c4e7d4146fa00d51aaaf95a273dca85a59
guilhermeMiguel123 <guilherme12miguel123@gmail.com>
2025-01-29 22:10:22 -0300

    novo comitt

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://guilhermemiguel123.github.io/Portifolio_react_vite_deploy/",
+  base: "/Portifolio_react_vite_deploy",
 })
