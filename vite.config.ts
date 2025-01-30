2d5a4d8792a0de85a1d418cf03ace6ce7fc05066
guilhermeMiguel123 <guilherme12miguel123@gmail.com>
2025-01-30 00:16:01 -0300

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Portifolio_react_vite_deploy",
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (
-          warning.code === "CSS_SYNTAX_ERROR" || // Ignora erros de CSS
-          warning.code === "JS_COMMENT_IN_CSS"  // Ignora "//" no CSS
-        ) {
-          return;
-        }
-        warn(warning);
-      },
-    },
-  },
-});
+})
